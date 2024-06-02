3588edce9ba8bec1b5cd66209c51035d620c1745
amirhossein dezhamkhah <dezhamkhah@gmail.com>
2024-06-02 15:22:40 +0330

    add change password

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django.core.mail.backends.console
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -135,6 +137,8 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # redirect when you login
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+
 
 
 
