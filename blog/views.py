1110c0d064d79056e87c61bbd421e968af15fe7d
amirhossein dezhamkhah <dezhamkhah@gmail.com>
2024-06-02 23:38:48 +0330

    read post -- crud

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, ListView
 from .models import Post
 from .forms import PostForm
 from django.urls import reverse_lazy
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model = Post
     template_name = 'home.html'
+    context_object_name = 'posts'
 
 
 class NewPost(CreateView):
